69782e43b3855ce80565413bd37a8b1322af360a
Alexander John <alexjohnpersonals@gmail.com>
2024-06-16 00:59:05 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+
+const Header = ({title}) => {
+  return (
+    <div>
+      <h1>{title}</h1>
+    </div>
+  )
+}
+
+const App = () => {
+
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const increaseGood = () => setGood(good + 1)
+  const increaseNeutral = () => setNeutral(neutral + 1)
+  const increaseBad = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <Header title='give feedback' />
+
+      <button onClick={increaseGood}>good</button>
+      <button onClick={increaseNeutral}>neutral</button>
+      <button onClick={increaseBad}>bad</button>
+
+      <Header title='statistics' />
+      
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
+}
+
+export default App
